3e256811962af4589e4fcbc52ffd4c79b9adfbeb
kaidouji85 <kaidouji85@gmail.com>
2024-05-10 08:17:53 +0000

    prettierインストール

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,15 @@
-import select, { Separator } from '@inquirer/select';
-import { Armdozers } from 'gbraver-burst-core';
+import select, { Separator } from "@inquirer/select";
+import { Armdozers } from "gbraver-burst-core";
 
 /** プレイヤー01のID */
 const player01Id = "player-01";
 
 /** プレイヤー02のID */
-const player02Id = "player-02"
+const player02Id = "player-02";
 
 /** アームドーザの選択肢 */
-const armdozerChoices = Armdozers.map(a => ({
-  value: a.id
+const armdozerChoices = Armdozers.map((a) => ({
+  value: a.id,
 }));
 
 /**
@@ -18,11 +18,11 @@ const armdozerChoices = Armdozers.map(a => ({
 (async () => {
   const player01Armdozer = await select({
     message: `select ${player01Id} armdozer`,
-    choices: armdozerChoices
+    choices: armdozerChoices,
   });
   const player02Armdozer = await select({
     message: `select ${player02Id} armdozer`,
-    choices: armdozerChoices
+    choices: armdozerChoices,
   });
   console.log(player01Armdozer, player02Armdozer);
 })();
