4b1492431a1f788754cc3edd6d0d1c6b0385a9fe
kaidouji85 <kaidouji85@gmail.com>
2024-05-10 08:13:18 +0000

    アームドーザ選択を追加した

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,28 @@
-console.log("hello gbraver burst cli v2");
+import select, { Separator } from '@inquirer/select';
+import { Armdozers } from 'gbraver-burst-core';
+
+/** プレイヤー01のID */
+const player01Id = "player-01";
+
+/** プレイヤー02のID */
+const player02Id = "player-02"
+
+/** アームドーザの選択肢 */
+const armdozerChoices = Armdozers.map(a => ({
+  value: a.id
+}));
+
+/**
+ * エントリポイント
+ */
+(async () => {
+  const player01Armdozer = await select({
+    message: `select ${player01Id} armdozer`,
+    choices: armdozerChoices
+  });
+  const player02Armdozer = await select({
+    message: `select ${player02Id} armdozer`,
+    choices: armdozerChoices
+  });
+  console.log(player01Armdozer, player02Armdozer);
+})();
