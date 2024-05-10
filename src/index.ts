974d22f953beb8dffaf0d7bf06f51e2c8a594189
kaidouji85 <kaidouji85@gmail.com>
2024-05-10 08:48:33 +0000

    プレイヤー選択を書き直した

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,55 @@
 import select from "@inquirer/select";
-import { Armdozers } from "gbraver-burst-core";
+import { Armdozers, Pilots, Player, PlayerId } from "gbraver-burst-core";
 
-/** プレイヤー01のID */
-const player01Id = "player-01";
-
-/** プレイヤー02のID */
-const player02Id = "player-02";
+/** ゲームに参加するプレイヤーIDをまとめたもの */
+const playerIds: [PlayerId, PlayerId] = ["player-01", "player-02"];
 
 /** アームドーザの選択肢 */
 const armdozerChoices = Armdozers.map((a) => ({
   value: a.id,
 }));
 
+/** パイロットの選択肢 */
+const pilotChoices = Pilots.map((p) => ({
+  value: p.id,
+}));
+
+/**
+ * アームドーザ、パイロットを選択する
+ * @param playerId 選択をするプレイヤーID
+ * @returns プレイヤー情報
+ */
+const playerSelect = async (playerId: PlayerId): Promise<Player> => {
+  const armdozerId = await select({
+    message: `select ${playerId} armdozer`,
+    choices: armdozerChoices,
+  });
+  const armdozer = Armdozers.find((a) => a.id === armdozerId);
+  if (!armdozer) {
+    throw new Error("armdozer not found");
+  }
+
+  const pilotId = await select({
+    message: `select ${playerId} pilot`,
+    choices: pilotChoices,
+  });
+  const pilot = Pilots.find((p) => p.id === pilotId);
+  if (!pilot) {
+    throw new Error("pilot not found");
+  }
+
+  return { playerId, armdozer, pilot };
+};
+
 /**
  * エントリポイント
  */
 (async () => {
-  const player01Armdozer = await select({
-    message: `select ${player01Id} armdozer`,
-    choices: armdozerChoices,
-  });
-  const player02Armdozer = await select({
-    message: `select ${player02Id} armdozer`,
-    choices: armdozerChoices,
-  });
-  console.log(player01Armdozer, player02Armdozer);
+  console.log("start gbraver burst cli");
+
+  const players: [Player, Player] = [
+    await playerSelect(playerIds[0]),
+    await playerSelect(playerIds[1]),
+  ];
+  console.log(players);
 })();
