0e8296f49f3aa7cc88499ede715607244977b68f
kaidouji85 <kaidouji85@gmail.com>
2024-05-10 09:51:12 +0000

    ゲームが一通り動くようになった

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,13 @@
 import select from "@inquirer/select";
 import {
   Armdozers,
+  GameState,
+  NoChoice,
   Pilots,
   Player,
+  PlayerCommand,
   PlayerId,
+  Selectable,
   startGBraverBurst,
 } from "gbraver-burst-core";
 import { EOL } from "os";
@@ -49,16 +53,51 @@ const playerSelect = async (playerId: PlayerId): Promise<Player> => {
 };
 
 /**
+ * コマンド選択
+ * @param option コマンド選択情報 
+ * @returns 選択したコマンド
+ */
+const commandSelect = async (
+  option: Selectable | NoChoice,
+): Promise<PlayerCommand> => {
+  const { playerId } = option;
+  if (!option.selectable) {
+    const { nextTurnCommand } = option;
+    return { playerId, command: nextTurnCommand };
+  }
+
+  const { command } = option;
+  const selectedCommand = await select({
+    message: `select ${playerId} command`,
+    choices: command.map((c) => ({
+      value: c,
+      name: JSON.stringify(c),
+    })),
+  });
+  return { playerId, command: selectedCommand };
+};
+
+/**
  * エントリポイント
  */
 (async () => {
   console.log("start gbraver burst cli" + EOL);
 
-  const players: [Player, Player] = [
+  const core = startGBraverBurst([
     await playerSelect(playerIds[0]),
     await playerSelect(playerIds[1]),
-  ];
-  const core = startGBraverBurst(players);
+  ]);
   console.log(EOL + "game start" + EOL);
-  console.dir(core.stateHistory(), { depth: null });
+  const initialStateHistory = core.stateHistory()
+  console.dir(initialStateHistory, { depth: null });
+
+  let lastState = initialStateHistory.at(-1);
+  while (lastState && lastState.effect.name === "InputCommand") {
+    const inputCommand = lastState.effect;
+    const updatedStateHistory = core.progress([
+      await commandSelect(inputCommand.players[0]), await commandSelect(inputCommand.players[1])
+    ]);
+    console.dir(updatedStateHistory, { depth: null });
+    lastState = updatedStateHistory.at(-1);
+  }
 })();
